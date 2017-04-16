86a6bd126f0896292e6ca885357e771bd58b93dd
Matan Kaminski <matan.kaminski.sdjasf@gmail.com>
2017-04-16 23:20:22 +0000

    Making run script more portable

--- run_vis.sh
+++ run_vis.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export CONFIG="config/plex_vis.cfg"
 
-export FLASK_APP=plex_vis.py
+APP_DIR=`dirname $0`
+export FLASK_APP=${APP_DIR}/plex_vis.py
+
 flask run --host=0.0.0.0 --with-threads
 
